c5bca151d76e3fcac9e02bf20db9669e7dcc1d00
Nelson Kuang <410374308@qq.com>
2017-11-10 18:22:30 +0800

    Create leetcode.js

---
+++ leetcode.js
@@ -0,0 +1,31 @@
+// https://leetcode.com/problems/median-of-two-sorted-arrays
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+var findMedianSortedArrays = function(nums1, nums2) {
+    var m = nums1.length,
+        n = nums2.length;
+    // edge handlers
+    if(m === 0){ 
+        if(n === 0){
+            return null;
+        }else if(n === 1){
+            return nums2[0];
+        } else if(n > 1){
+            return (nums2[0] + nums2[n - 1] / 2).toFixed(1, 10);
+        }
+    }
+    if(n === 0){
+        if(m === 0){
+            return null;
+        }else if(m === 1){
+            return nums1[0];
+        } else if(m > 1){
+            return (nums1[0] + nums1[m - 1] / 2).toFixed(1, 10);
+        }        
+    }
+    // other handlers
+    return ((nums1[0] + nums1[m - 1] + nums2[0] + nums2[n - 1]) / 4).toFixed(1, 10);
+};
